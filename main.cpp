7d8e229b6c5a69097d85fc988a3415abf0d63221
theoph35 <theophile.dechelotte@mines-paristech.fr>
2022-01-28 11:54:22 +0100

    Ajout init_Grille

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 # include "mechant.h"
 #include "game.h"
+#include "grille.h"
+
+
+void init_Grille() {
+    """
+    Fonction permettant de reinitialiser/initialiser la grille en ne construisant que le background, le ground, les murs, et le couloir
+    """
+    std::vector <std::vector <char> > grille (LINES, std::vector <char> (COLUMNS,BACKGROUND));
+    grille.Ground();
+    grille.vertical_Walls();
+    grille.horizontal_Walls();
+    grille.Corridor();
+}
 
 
 int main(){
