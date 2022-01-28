3fb46f880f3345faeb19ef813f10d0bfbb9cefcf
theoph35 <theophile.dechelotte@mines-paristech.fr>
2022-01-28 13:35:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,6 @@
 #include "grille.h"
 
 
-void init_Grille() {
-    """
-    Fonction permettant de reinitialiser/initialiser la grille en ne construisant que le background, le ground, les murs, et le couloir
-    """
-    std::vector <std::vector <char> > grille (LINES, std::vector <char> (COLUMNS,BACKGROUND));
-    grille.Ground();
-    grille.vertical_Walls();
-    grille.horizontal_Walls();
-    grille.Corridor();
-}
-
 
 int main(){
     Game g;
