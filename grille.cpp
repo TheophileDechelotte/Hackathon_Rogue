aa11260ea51aeeda527557ba451760d2dfe73ad4
theoph35 <theophile.dechelotte@mines-paristech.fr>
2022-01-28 10:43:04 +0100

    Update grille.cpp

--- grille.cpp
+++ grille.cpp
@@ -15,7 +15,10 @@ playground(playground)
 {}
 
 void Grille::draw_Playground(const std::vector <std::vector <char> > & playground) {
-    for (int i=0 ; i<int(playground.size()/3) ; i++) {
+    """
+    Fonction permettant d'afficher la grille que l'on a construit
+    """
+    for (int i=0 ; i<playground.size() ; i++) {
         for (int j=0 ; j<playground[1].size() ; j++) {
             std::cout << playground[i][j];
         }
@@ -23,12 +26,36 @@ void Grille::draw_Playground(const std::vector <std::vector <char> > & playgroun
     }
 }
 
-void vertical_Walls(const std::vector <std::vector <char> > & playground) {
-    for (int j=0 ; j<(playground[1].size() ; j++) {
+void Grille::vertical_Walls(const std::vector <std::vector <char> > & playground) {
+    """
+    Fonction permettant d'ajouter à la grille les murs verticaux
+    """
+    
+    for (int j=0 ; j<int(playground[1].size()/3) ; j++) {
+        playground[0][j] = '_';
+        playground[playground.size()-1][j] = '_';
+    }
+    
+    for (int j=int(2*playground[1].size()/3) ; j<playground[1].size() ; j++) {
         playground[0][j] = '_';
         playground[playground.size()-1][j] = '_';
     }
 }
+
+void Grille::horizontal_Walls(const std::vector <std::vector <char> > & playground) {
+    """
+    Fonction permettant d'ajouter à la grille les murs horizontaux
+    """
+    
+    for (int i=1 ; i<playground.size() ; i++) {
+        playground[i][0] = '|';
+        playground[i][playground[1].size()/3] = '|';
+        playground[i][2*playground[1].size()/3] = '|';
+        playground[i][playground[1].size()-1] = '|';
+    }
+}
+
+
 
 
 
