58d10d49128ce5010d5cfa596a3040f2a2a6ea94
theoph35 <theophile.dechelotte@mines-paristech.fr>
2022-01-28 10:27:20 +0100

    Update grille.cpp

--- grille.cpp
+++ grille.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <vector>
+#include "grille.h"
+
+
 
 const int LINES = 10;
 const int COLUMNS = 10;
 const char GROUND = '.';
 const char CORRIDOR = '#';
 const char DOOR = '+';
+
+Grille::Grille(const std::vector <std::vector <char> > playground) :
+playground(playground)
+{}
+
+void Grille::draw_Playground(const std::vector <std::vector <char> > & playground) {
+    for (int i=0 ; i<playground.size() ; i++) {
+        for (int j=0 ; j<playground[1].size() ; j++) {
+            std::cout << playground[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+
 
 
 
