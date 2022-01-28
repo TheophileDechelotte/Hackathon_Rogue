4e4059122ae5b5c349f132ebd088fe71ca0164fc
Olyem <olympe.tournier@outlook.fr>
2022-01-28 09:55:13 +0100

    ajout attributs nom, x, y

--- mechant.h
+++ mechant.h
@@ -1,3 +1,12 @@
+# include <vector>
+# include <string>
+
 class Mechant{
+    private :
+        std::string nom ;
+        int x ;
+        int y ;
+    public :
+        Mechant (std::string nom, int x, int y) ;
 
 } ;
