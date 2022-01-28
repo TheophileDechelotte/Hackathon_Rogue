91e8e866b3ae99c4aab85c985bc2a8b9b6e9e283
theoph35 <theophile.dechelotte@mines-paristech.fr>
2022-01-28 15:26:22 +0100

    Update perso.cpp

--- perso.cpp
+++ perso.cpp
@@ -23,22 +23,22 @@ void attrape_cle() {}
 
 void Perso::marche(char dir, Grille& grille){
     if(dir == 'k'){
-        if (grille.at(x+1,y)==2 || grille.at(x+1,y)==5){
+        if (grille.at(x+1,y)==2 || grille.at(x+1,y)==5 || grille.at(x+1,y)==8){
             x++;
         }
     }
     if(dir == 'i'){
-        if (grille.at(x-1,y)==2 || grille.at(x-1,y)==5){
+        if (grille.at(x-1,y)==2 || grille.at(x-1,y)==5 || grille.at(x-1,y)==8){
             x = x-1;
         }
     }
     if(dir == 'l'){
-        if (grille.at(x,y+1)==2 || grille.at(x,y+1)==5){
+        if (grille.at(x,y+1)==2 || grille.at(x,y+1)==5 || grille.at(x,y+1)==8){
             y++;
         }
     }
     if(dir == 'j'){
-        if (grille.at(x,y-1)==2 || grille.at(x,y-1)==5){
+        if (grille.at(x,y-1)==2 || grille.at(x,y-1)==5 || grille.at(x,y-1)==8){
             y = y-1;
         }
     }
