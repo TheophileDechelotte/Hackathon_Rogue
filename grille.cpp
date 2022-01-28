5b06b289e8b5079ee76de307a24df1803f8b719f
theoph35 <theophile.dechelotte@mines-paristech.fr>
2022-01-28 11:32:31 +0100

    Update grille.cpp

--- grille.cpp
+++ grille.cpp
@@ -3,24 +3,25 @@
 #include "grille.h"
 
 
-
 const int LINES = 10;
-const int COLUMNS = 10;
+const int COLUMNS = 30;
+const char BACKGROUND ' ';
 const char GROUND = '.';
 const char CORRIDOR = '#';
 const char DOOR = '+';
 
-Grille::Grille(const std::vector <std::vector <char> > playground) :
-playground(playground)
+
+Grille::Grille(const std::vector <std::vector <char> > grille) :
+grille(grille)
 {}
 
-void Grille::draw_Playground() {
+void Grille::draw_Grille() {
     """
     Fonction permettant d'afficher la grille que l'on a construit
     """
-    for (int i=0 ; i<playground.size() ; i++) {
-        for (int j=0 ; j<playground[1].size() ; j++) {
-            std::cout << playground[i][j];
+    for (int i=0 ; i<grille.size() ; i++) {
+        for (int j=0 ; j<grille[1].size() ; j++) {
+            std::cout << grille[i][j];
         }
         std::cout << std::endl;
     }
@@ -31,11 +32,11 @@ void Grille::vertical_Walls() {
     Fonction permettant d'ajouter à la grille les murs verticaux
     """
     
-    for (int i=1 ; i<playground.size() ; i++) {
-        playground[i][0] = '|';
-        playground[i][int(playground[1].size()/3)] = '|';
-        playground[i][int(2*playground[1].size()/3)] = '|';
-        playground[i][playground[1].size()-1] = '|';
+    for (int i=1 ; i<grille.size() ; i++) {
+        grille[i][0] = '|';
+        grille[i][int(grille[1].size()/3)] = '|';
+        grille[i][int(2*grille[1].size()/3)] = '|';
+        grille[i][grille[1].size()-1] = '|';
     }
 }
 
@@ -44,24 +45,28 @@ void Grille::horizontal_Walls() {
     Fonction permettant d'ajouter à la grille les murs horizontaux
     """
     
-    for (int j=0 ; j<int(playground[1].size()/3) ; j++) {
-        playground[0][j] = '_';
-        playground[playground.size()-1][j] = '_';
+    for (int j=0 ; j<int(grille[1].size()/3) ; j++) {
+        grille[0][j] = '_';
+        grille[grille.size()-1][j] = '_';
     }
     
-    for (int j=int(2*playground[1].size()/3) ; j<playground[1].size() ; j++) {
-        playground[0][j] = '_';
-        playground[playground.size()-1][j] = '_';
+    for (int j=int(2*grille[1].size()/3) ; j<grille[1].size() ; j++) {
+        grille[0][j] = '_';
+        grille[grille.size()-1][j] = '_';
     }
 }
 
 void Grille::Corridor() {
+    """
+    Fonction permettant d'ajouter à la grille le couloir
+    """
 
-
-    for (int j=int(playground[1].size()/3) ; j<int(2*playground[1].size()/3) ; j++) {
-        playground[int(playground.size()/2)][j] ;
+    for (int j=int(grille[1].size()/3) ; j<int(2*grille[1].size()/3) ; j++) {
+        grille[int(grille.size()/2)][j] ;
     }
 }
+
+
 
 
 
